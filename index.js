30ba8086cd66eecbfbe71ce72e15bc1c355d7218
devtenmaster <devtenmaster@gmail.com>
2024-09-20 16:58:07 +0900

    123

--- index.js
+++ index.js
@@ -63,10 +63,10 @@ window.addEventListener("load", function () {
     meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
       document.getElementsByTagName('head')[0].appendChild(meta);
 
-      screen.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone screen lock error") });
-      canvas.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone canvas lock error") });
-      window.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone window lock error") });
-      container.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone container lock error") });
+      screen.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone screen lock error : " + error) });
+      canvas.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone canvas lock error : " + error) });
+      window.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone window lock error : " + error) });
+      container.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone container lock error : " + error) });
       //config.matchWebGLToCanvasSize = false;
   }
 
@@ -82,10 +82,10 @@ window.addEventListener("load", function () {
         unityInstanceRef = unityInstance;
         loadingBar.style.display = "none";
 
-        //screen.orientation.lock("portrait").then(() => { }).catch((error) => { alert("onload screen lock error") });
-        //canvas.orientation.lock("portrait").then(() => { }).catch((error) => { alert("onload canvas lock error") });
-        //window.orientation.lock("portrait").then(() => { }).catch((error) => { alert("onload window lock error") });
-        //container.orientation.lock("portrait").then(() => { }).catch((error) => { alert("onload container lock error") });
+        //screen.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone screen lock error : " + error) });
+        //canvas.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone canvas lock error : " + error) });
+        //window.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone window lock error : " + error) });
+        //container.orientation.lock("landscape").then(() => { }).catch((error) => { alert("phone container lock error : " + error) });
     }).catch((message) => {
       alert(message);
     });
